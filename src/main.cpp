6d37d58397a9da7f4199e3d87afb50d365c7b664
Lorenz Saalmann <karl.lorenz.saalmann@physik.uni-giessen.de>
2025-11-26 16:23:25 +0100

    + added imgui demo window

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,76 @@
 #include <GLFW/glfw3.h>
 #include <imgui.h>
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 int main() {
-    GLFWwindow* window;
-
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // macOS requirement
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "ImGui Test", nullptr, nullptr);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // enable vsync
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    // --------- ImGui SETUP ----------
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    const ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
+    // --------------------------------
+
+    bool show_demo = true;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // --------- TEST UI ----------
+        ImGui::Begin("ImGui Working!");
+        ImGui::Text("✅ If you see this, ImGui works!");
+        ImGui::Checkbox("Show Demo Window", &show_demo);
+        ImGui::End();
+
+        if (show_demo)
+            ImGui::ShowDemoWindow(&show_demo);
+        // ----------------------------
+
+        // Render
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        glfwSwapBuffers(window);
     }
 
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
