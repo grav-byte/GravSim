6b22f8dd3d539446a2c093da111dfce8d207f386
Lorenz Saalmann <karl.lorenz.saalmann@physik.uni-giessen.de>
2025-12-10 11:22:50 +0100

    + physics dir

---
+++ src/App/Engine/Physics/PhysicsSolver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lorenz Saalmann on 10.12.25.
+//
+
+#include "PhysicsSolver.h"
---
+++ src/App/Engine/Physics/PhysicsSolver.h
@@ -0,0 +1,9 @@
+//
+// Created by Lorenz Saalmann on 10.12.25.
+//
+
+#pragma once
+
+
+class PhysicsSolver {
+};
