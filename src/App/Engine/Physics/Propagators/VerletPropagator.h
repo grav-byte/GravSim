7920e15a06512ae959c2857b6d7deecdf3b36160
Lorenz Saalmann <karl.lorenz.saalmann@physik.uni-giessen.de>
2025-12-10 16:07:35 +0100

    + merged verlet

--- src/App/Engine/Physics/Propagators/VerletPropagator.h
+++ src/App/Engine/Physics/Propagators/VerletPropagator.h
@@ -1,12 +1,11 @@
 //
-// Created by Lorenz Saalmann on 10.12.25.
+// Created by kw on 10.12.2025.
 //
 
 #pragma once
 #include "IPropagator.h"
 
-
-class VerletPropagator: public IPropagator {
+class VerletPropagator : public IPropagator {
 public:
-    void Propagate(SceneObject &object, glm::vec2 acceleration, float deltaTime) override;
+    void Propagate(SceneObject& object, glm::vec2 acceleration, float deltaTime) override;
 };
