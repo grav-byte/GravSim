4b0c0e04a34c9f359bb64e7ea7c67738487ecb81
Lorenz Saalmann <karl.lorenz.saalmann@physik.uni-giessen.de>
2026-01-06 10:38:23 +0100

    + fixed PID error

--- src/App/RocketControl/PIDController.cpp
+++ src/App/RocketControl/PIDController.cpp
@@ -17,11 +17,15 @@ PIDController::PIDController(const float p, const float i, const float d) {
 float PIDController::Evaluate(const float setpoint, const float measuredValue, const float derivative,
                               const float deltaTime) {
     const float error = setpoint - measuredValue;
+
     const float p = pGain * error;
 
     integral_ += error * deltaTime;
+    const float iMax = 1.0f / glm::max(iGain, 0.0001f); // prevent division by zero
+    integral_ = glm::clamp(integral_, -iMax, iMax);
     const float i = iGain * integral_;
-    const float d = dGain * derivative;
+
+    const float d = -dGain * derivative;
     const float result = p + i + d;
 
     return glm::clamp(result, -1.0f, 1.0f);
