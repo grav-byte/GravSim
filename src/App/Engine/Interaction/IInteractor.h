834eb2080de143f0e7b51b0a15870e658b873043
Lorenz Saalmann <karl.lorenz.saalmann@physik.uni-giessen.de>
2026-01-12 21:22:19 +0100

    + windows imgui fix

--- src/App/Engine/Interaction/IInteractor.h
+++ src/App/Engine/Interaction/IInteractor.h
@@ -9,12 +9,12 @@ public:
     virtual void DrawPreview(const Scene &scene, glm::vec2 mousePos) {
         // default preview: draw circle at mouse position with radius
         constexpr int segments = 64;
-        ImDrawList* drawList = ImGui::GetForegroundDrawList();
+        ImDrawList* drawList = ImGui::GetForegroundDrawList(ImGui::GetMainViewport());
         const auto cam = scene.GetCamera();
         const glm::vec2 origin = cam->WorldToScreen(glm::vec2(0.0f));
         const glm::vec2 radiusWorld = cam->WorldToScreen(glm::vec2(0.0f, radius));
         const float screenRadius = glm::distance(origin, radiusWorld);
-        drawList->AddCircle(ImVec2(mousePos.x, mousePos.y), screenRadius, IM_COL32(255, 255, 255, 255), segments, 2.0f);
+        drawList->AddCircle(ImGui::GetMousePos(), screenRadius, IM_COL32(255, 255, 255, 255), segments, 2.0f);
     }
 
     virtual void Interact(Scene &scene, glm::vec2 mousePos, bool leftMouse) = 0;
