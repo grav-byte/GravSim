c9b33fa2d9a466f6c3e26ef4059fc74815cedccc
Lorenz Saalmann <karl.lorenz.saalmann@physik.uni-giessen.de>
2026-01-13 16:14:47 +0100

    + added appicon

--- src/App/UI/FileSelector.cpp
+++ src/App/UI/FileSelector.cpp
@@ -7,6 +7,7 @@ FileSelector::FileSelector(std::string path): directory_(std::move(path)), selec
 
 void FileSelector::RefreshFiles() {
     files_.clear();
+    auto p = std::filesystem::current_path() ;
     for (const auto& entry : fs::directory_iterator(directory_)) {
         if (entry.is_regular_file()) {
             files_.push_back(entry.path().filename().string());
